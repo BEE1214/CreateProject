7f828791552b7b0d133cba989b01880163f445fb
BEE1214 <advorsky.gos@gmail.com>
2019-06-16 21:26:25 +0200

    Added file templates

---
+++ Templates/CPP.cpp
@@ -0,0 +1,14 @@
+//=================================//
+//=-Project: $argv[1]-------------=//
+//=-Author: Adam Dvorsky----------=//
+//=-Date: $date ------------------=//
+//=================================//
+// #include <stdio.h>
+#include <iostream>
+
+
+int main(void)
+{
+
+  return 0;
+}
---
+++ Templates/PYTHON.py
@@ -0,0 +1,15 @@
+##=================================##
+##=-Project: $argv[1]-------------=##
+##=-Author: Adam Dvorsky----------=##
+##=-Date: $date ------------------=##
+##=================================##
+
+import sys
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    # Source code
+    main()
+    pass
