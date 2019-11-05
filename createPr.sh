9c633f4be15a6259c6dee8044376c61b94a4bee1
BEE1214 <advorsky.gos@gmail.com>
2019-11-05 12:28:02 +0100

    File control doesn't work

--- createPr.sh
+++ createPr.sh
@@ -2,22 +2,34 @@
 
 function createPr () {
     # Check if arguments aren't empty
-    if [ -z "$2" ]; then 
+    FOLDER=/home/adam/Documents/FunProjects/$1
+    FILE=$1.$2
+
+    if [ -z "$1" ]; then 
+        echo No arguments entered...
+        echo Exiting...
+    elif [ -z "$2" ]; then
         echo Second argument is empty...
         echo Exiting...
     else
-        if [ ! -d "$~/Documents/FunProjects/$1" ]; then
+        if [ ! -d "$FOLDER" ]; then
             mkdir ~/Documents/FunProjects/$1
             echo Creating folder $1...
             cd ~/Documents/FunProjects/$1
         else
-            echo Moving to folde $1...
+            echo Moving to folder $1...
             cd ~/Documents/FunProjects/$1
         fi
 
         python ~/Documents/FunProjects/CreateProject/createPr.py $1 $2
-        chmod 744 $1.$2
-        lsd -l --group-dirs first
-        code .
+        
+        if [ -f "$FILE"]; then
+            chmod 744 $1.$2
+            lsd -l --group-dirs first
+            code .
+        else
+            echo File $FILE does not exist...
+            echo Exiting...
+        fi
     fi
 }
